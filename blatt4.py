using variance and corrected use of activator function

blatt4.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot
 
+from k_means import k_means, variance, create_clusters, choose_point
 from neural_net import *
 
 def to_polar(coordinates):
@@ -42,30 +43,52 @@ def reshape_grid(grid, xshape, yshape):
 
 grid = make_grid(x, y)
 
-random_weights = np.stack((
-    np.random.sample(25) * (x[-1] - x[0]) + x[0],
-    np.random.sample(25) * (y[-1] - y[0]) + y[0]))
-
-net = NeuralNet(2)
-net.add_layer(DistanceLayer(random_weights, gauss))
-net.add_layer(SumLayer(25, 1, np.tanh, tanh_back))
-
 samples = np.vstack((class1.T, class2.T))
 targets1 = np.ones((class1.shape[1], 1))
 targets2 = -np.ones((class2.shape[1], 1))
 targets = np.vstack((targets1, targets2))
 
-avg_error = learn_random_order(net, samples, targets, 1000, .0001)
+# weights = np.stack((
+#     np.random.sample(25) * (x[-1] - x[0]) + x[0],
+#     np.random.sample(25) * (y[-1] - y[0]) + y[0]))
+points = []
+for i in range(25):
+    points.append(choose_point(samples, points))
+weights = np.array(points).T
+
+clusters = create_clusters(samples, weights.T)
+# variances = np.array([variance(samples, p) for p in weights.T])
+variances = np.array([variance(clusters[i], weights.T[i]) for i in range(len(clusters))])
+
+# means1, variances1 = k_means(class1.T, 12, init_k=4)
+# means2, variances2 = k_means(class2.T, 13, init_k=5)
+# weights = np.hstack((np.array(means1).T, np.array(means2).T))
+# variances = np.hstack((variances1, variances2))
+# variances = np.array([variance(samples, p) for p in weights.T])
+
+# means = k_means(samples, 25, init_k=5)
+# weights, variances = np.array(means[0]).T, np.array(means[1])
+#
+# variances = np.array(np.ones((1, weights.shape[1])))
+
+net = NeuralNet(2)
+net.add_layer(DistanceLayer(weights, variances, gauss))
+net.add_layer(SumLayer(25, 1, np.tanh, tanh_back))
+
+avg_error = learn_random_order(net, samples, targets, 10000, .0001)
 
 draw_error(avg_error, 'avg error')
+
+correctly_classified = np.sum(np.abs(net.calculate(samples) + targets) > 1)
 
 result = net.calculate(grid)
 
 pyplot.axes(xlim=(x[0], x[-1]), ylim=(y[0], y[-1]))
-cs = pyplot.contourf(x, y, reshape_grid(result, x.shape[0], y.shape[0]))
+cs = pyplot.contourf(x, y, reshape_grid(result, x.shape[0], y.shape[0]), levels = [-2, 0, 2])
 # pyplot.clabel(cn, inline=1, fontsize=6)
 pyplot.colorbar(cs)
 pyplot.plot(class1[0], class1[1], '.', color='blue')
 pyplot.plot(class2[0], class2[1], '.', color='red')
-pyplot.plot(random_weights[0], random_weights[1], 'x')
+pyplot.plot(weights[0], weights[1], 'x')
+pyplot.suptitle('correctly classified: {} / {}'.format(correctly_classified, samples.shape[0]))
 pyplot.show()

k_means.py
@@ -1,50 +1,69 @@
 import random
 import numpy as np
 
-def k_means(points, k):
-    midpoints = [weighted_middle(points)]
-    if k % 2 != 1:
-        raise AttributeError('this implementation needs an uneven number of points')
+def k_means(points, k, init_k=1):
+    if (k - init_k) % 2 != 0:
+        raise AttributeError('k - init_k needs to be even')
+    midpoints = []
+    for i in range(init_k):
+        midpoints.append(choose_point(points, midpoints))
     while len(midpoints) < k:
         clusters = create_clusters(points, midpoints)
         midpoints = [weighted_middle(cluster) for cluster in clusters]
-        _, greatest_error = max(clusters, lambda c, i: error(c, midpoints[i]))
-        _, most_elements = max(clusters, lambda c, i: len(c))
+        _, greatest_error = find(clusters, lambda c, i: variance(c, midpoints[i]), max_comp)
+        _, most_elements = find(clusters, lambda c, i: len(c), max_comp)
         midpoints.append(choose_point(clusters[greatest_error], midpoints))
         midpoints.append(choose_point(clusters[most_elements], midpoints))
     last_midpoints = midpoints
     clusters = create_clusters(points, midpoints)
     midpoints = [weighted_middle(cluster) for cluster in clusters]
+    while not np.array_equal(last_midpoints, midpoints):
+        last_midpoints = midpoints
+        clusters = create_clusters(points, midpoints)
+        midpoints = [weighted_middle(cluster) for cluster in clusters]
+    return midpoints, [variance(clusters[i], midpoints[i]) for i in range(len(clusters))]
 
 
 def choose_point(points, not_in):
     new = random.choice(points)
-    while new in not_in:
+    while any(np.array_equal(new, p) for p in not_in):
         new = random.choice(points)
     return new
 
 
-def error(cluster, midpoint):
-    return np.linalg.norm(np.array(cluster) - midpoint)**2
+def variance(cluster, midpoint):
+    if len(cluster) == 0:
+        return 1
+    else:
+        return np.sum(np.linalg.norm(np.array(cluster) - midpoint, axis=1)**2) / len(cluster)
 
 
-def max(list, func):
+def max_comp(a, b):
+    return a > b
+
+
+def min_comp(a,b):
+    return a < b
+
+
+def find(list, func, comparer):
     max = 0
-    max_value = func(list[0])
+    max_value = func(list[0], 0)
     for i in range(1, len(list)):
-        if func(list[i]) > max_value:
+        val = func(list[i], i)
+        if comparer(val, max_value):
             max = i
-            max_value = func(list[i], i)
+            max_value = val
     return max_value, max
 
 
 def create_clusters(points, midpoints):
-    clusters = [[]] * len(midpoints)
+    clusters = [[] for _ in range(len(midpoints))]
     for p in points:
-        _, nearest = max(midpoints, lambda mp, i: np.linalg.norm(p, mp))
+        _, nearest = find(midpoints, lambda mp, i: np.linalg.norm(p - mp), min_comp)
         clusters[nearest].append(p)
     return clusters
 
 
 def weighted_middle(points):
-    return np.sum(points, axis=0) / points.shape[0]
+    return np.sum(points, axis=0) / len(points)

neural_net.py
@@ -50,8 +50,11 @@ def average_quadratic_error(computed, targets):
     return np.sum(quadratic_error(computed, targets)) / computed.shape[0]
 
 
-def gauss(integrated):
-    return np.exp(-integrated ** 2 / 2)
+def gauss(integrated, variance):
+    intermediate = -(integrated ** 2) / (2 * variance ** 2)
+    result = np.zeros(integrated.shape)
+    result[np.abs(intermediate) < 100] = np.exp(intermediate[np.abs(intermediate) < 100])
+    return result
 
 
 class Layer:
@@ -101,16 +104,18 @@ class SumLayer(Layer):
 
 
 class DistanceLayer(Layer):
-    def __init__(self, weights, activator):
+    def __init__(self, weights, variances, activator):
         self.weights = weights
         self.activator = activator
+        self.variances = variances
 
     def propagate(self, inputs):
         return self.calculate(inputs)
 
     def calculate(self, inputs):
         s = self.weights.shape
-        return np.array([np.linalg.norm(inp.reshape((s[0], 1)) @ np.ones((1, s[1])) - self.weights, axis=0) for inp in inputs])
+        distances = np.array([np.linalg.norm(inp.reshape((s[0], 1)) @ np.ones((1, s[1])) - self.weights, axis=0) for inp in inputs])
+        return self.activator(distances, self.variances)
 
     def back_propagate(self, back_inputs):
         pass
@@ -183,7 +188,7 @@ def draw_error(avg_error, title, path = None, file = 'avg_error.png'):
     # show development of quadratic error
     generations = len(avg_error)
     gen_space = np.arange(0, generations, 1)
-    pyplot.axes(xlim=(0, generations - 1), yscale='log', ylim=(0.01, 1))
+    pyplot.axes(xlim=(0, generations - 1), yscale='log')
     pyplot.plot(gen_space, avg_error)
     pyplot.suptitle('{}\ndevelopment of average quadratic error\nlog scale'.format(title))
     if path is not None: